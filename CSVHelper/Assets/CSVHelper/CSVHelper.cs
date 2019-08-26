c665e6f158e7daa26818e113779c4957f41d88b0
ChuKuang <w199329z@163.com>
2019-08-26 20:00:50 +0800

    Update CSVHelper.cs
    扩充int数组类型和string数组类型的支持，修改方法名

--- CSVHelper/Assets/CSVHelper/CSVHelper.cs
+++ CSVHelper/Assets/CSVHelper/CSVHelper.cs
@@ -9,7 +9,7 @@ namespace WZFrame
 {
     public class CSVHelper
     {
-        public static Dictionary<int, T> GetDataTable<T>(string csvTableStr) where T : CSVDataBase, new()
+        public static Dictionary<int, T> GetTableData<T>(string csvTableStr) where T : CSVDataBase, new()
         {
             string content = csvTableStr.Replace("\r", "");
             string[] lines = content.Split('\n');
@@ -41,13 +41,21 @@ namespace WZFrame
                         {
                             pins[j].SetValue(data, GetInt(strDic[pins[j].Name][i -2]), null);
                         }
-                        if (type == typeof(float))
+                        else if (type == typeof(float))
                         {
                             pins[j].SetValue(data, GetFloat(strDic[pins[j].Name][i - 2]), null);
                         }
-                        if (type == typeof(bool))
+                        else if(type == typeof(bool))
                         {
                             pins[j].SetValue(data, GetBool(strDic[pins[j].Name][i - 2]), null);
+                        }
+                        else if(type == typeof(string[]))
+                        {
+                            pins[j].SetValue(data, GetStringArray(strDic[pins[j].Name][i - 2]), null);
+                        }
+                        else if (type == typeof(int[]))
+                        {
+                            pins[j].SetValue(data, GetIntArray(strDic[pins[j].Name][i - 2]), null);
                         }
                         else
                         {
@@ -63,10 +71,16 @@ namespace WZFrame
             return dic;
         }
 
-        // 生成的第一行是类型（int， string等）
-        //第二行属性名字，对应类属性名
-        //第三行开始是属性值
-        //将返回的字符串保存文本就是完整csv格式了
+
+        /// <summary>
+        /// 序列化字典数据为CSV字符串格式
+        /// 生成的第一行是类型（int， string等）
+        /// 第二行属性名字，对应类属性名
+        /// 第三行开始是属性值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dictionary"></param>
+        /// <returns></returns>
         public static string GetCSVContent<T>(Dictionary<int, T> dictionary) where T : CSVDataBase, new()
         {
             string content = string.Empty;
@@ -95,6 +109,10 @@ namespace WZFrame
                     System.Object value = item.GetValue(data, null);
                     if (data == null)
                         value = string.Empty;
+                    else if (item.PropertyType == typeof(string[]))
+                        value = SetArrayToString(value);
+                    else if (item.PropertyType == typeof(int[]))
+                        value = SetArrayToInt(value);
 
                     content += (value.ToString() + ",").Trim();
                 }
@@ -132,7 +150,11 @@ namespace WZFrame
             return dic;
         }
 
-
+        /// <summary>
+        /// 反序列化int
+        /// </summary>
+        /// <param name="dText"></param>
+        /// <returns></returns>
         public static int GetInt(string dText)
         {
             if (string.IsNullOrEmpty(dText))
@@ -141,6 +163,12 @@ namespace WZFrame
                 return (int)float.Parse(dText);
         }
 
+
+        /// <summary>
+        /// 反序列化string
+        /// </summary>
+        /// <param name="dText"></param>
+        /// <returns></returns>
         public static string GetString(string dText)
         {
             if (string.IsNullOrEmpty(dText))
@@ -149,6 +177,11 @@ namespace WZFrame
                 return dText;
         }
 
+        /// <summary>
+        /// 反序列化float
+        /// </summary>
+        /// <param name="dText"></param>
+        /// <returns></returns>
         public static float GetFloat(string dText)
         {
             if (string.IsNullOrEmpty(dText))
@@ -157,9 +190,13 @@ namespace WZFrame
                 return float.Parse(dText);
         }
 
-
-        //这里我当表里的是"true", "false"
-        //如果是用0 和1代替的需要更改
+        /// <summary>
+        /// 反序列化bool
+        /// 这里我当表里的是"true", "false"
+        //、如果是用0 和1代替的需要更改
+        /// </summary>
+        /// <param name="dText"></param>
+        /// <returns></returns>
         public static bool GetBool(string dText)
         {
             if (string.IsNullOrEmpty(dText))
@@ -169,6 +206,98 @@ namespace WZFrame
         }
 
 
+        /// <summary>
+        /// 反序列化int[]
+        /// </summary>
+        /// <param name="dText"></param>
+        /// <returns></returns>
+        public static int[] GetIntArray(string dText)
+        {
+            string[] data;
+            if (string.IsNullOrEmpty(dText))
+                data = new string[] { };
+            else
+                data = dText.Split('#');
+
+            int[] intArray = new int[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                intArray[i] = int.Parse(data[i]);
+            }
+            return intArray;
+        }
+
+
+        /// <summary>
+        /// 反序列化string[]
+        /// </summary>
+        /// <param name="dText"></param>
+        /// <returns></returns>
+        public static string[] GetStringArray(string dText)
+        {
+            string[] data;
+            if (string.IsNullOrEmpty(dText))
+                data = new string[] { };
+            else
+                data = dText.Split('#');
+
+            string[] attrsArray = new string[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(data[i]))
+                    attrsArray[i] = data[i];
+                else
+                    attrsArray[i] = string.Empty;
+            }
+
+            return attrsArray;
+        }
+
+
+
+        /// <summary>
+        /// 序列化位表里的string[],加#
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static string SetArrayToString(object array)
+        {
+            string[] stringArray = (string[])array;
+            string stringValue = string.Empty;
+            if(stringArray != null &&  stringArray.Length > 0)
+            {
+                foreach(var item in stringArray)
+                {
+                    if (string.IsNullOrEmpty(item))
+                        continue;
+                    stringValue += item + "#";
+                }
+                stringValue = stringValue.Remove(stringValue.Length - 1);
+            }
+
+            return stringValue;
+        }
+
+        /// <summary>
+        /// 序列化位表里的Int[],加#
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static string SetArrayToInt(object array)
+        {
+            int[] intArray = (int[])array;
+            string stringValue = string.Empty;
+            if(intArray != null && intArray.Length > 0)
+            {
+                foreach (var item in intArray)
+                {
+                    stringValue += item + "#";
+                }
+                stringValue = stringValue.Remove(stringValue.Length - 1);
+            }
+            return stringValue;
+        }
+
         public static string GetTypeName(string type)
         {
             string name = string.Empty;
@@ -177,8 +306,14 @@ namespace WZFrame
                 case "Int32":
                     name = "int";
                     break;
+                case "Int32[]":
+                    name = "int[]";
+                    break;
                 case "String":
                     name = "string";
+                    break;
+                case "String[]":
+                    name = "string[]";
                     break;
                 case "Single":
                     name = "float";
@@ -189,6 +324,7 @@ namespace WZFrame
                 default:
                     name = type;
                     break;
+                
             }
 
             return name;
